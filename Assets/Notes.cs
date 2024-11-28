32ee7d8536f83dc19dca224f5d84991a7b42a3b7
netch-dev <daijeln361@gmail.com>
2024-11-27 21:55:39 -0800

    Object Pooling

--- Assets/Notes.cs
+++ Assets/Notes.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -180,6 +181,169 @@ namespace Netch.GameProgrammingPatterns {
 
 			private bool CanSeeEnemy() {
 				return true;
+			}
+		}
+		#endregion
+
+		#region Object Pooling
+		// - Object pooling is a design pattern that allows you to reuse objects instead of creating and destroying them
+		// - Saving memory and CPU cycles
+		// - Useful for objects that are frequently created and destroyed
+
+		// - Reusing objects is faster than creating new ones
+		// - This is mostly done by turning the object off and moving it to a new location
+
+
+		// -------------------------------
+		// -------------------------------
+		// -------------------------------
+
+		// - Simple pool for a single object:
+		// - The problem with this approach is that it only works for a single object type, and when the project gets bigger there will be more object types. Each requiring its own pool
+
+		// Create a gameobject and attach this script to it
+		public class NPCPool_Simple : MonoBehaviour {
+			[SerializeField] private GameObject enemyPrefab;
+			[SerializeField] private Queue<GameObject> enemyPool = new Queue<GameObject>();
+			[SerializeField] private int poolStartSize = 5;
+
+			private void Start() {
+				for (int i = 0; i < poolStartSize; i++) {
+					GameObject enemy = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
+					enemy.SetActive(false);
+					enemyPool.Enqueue(enemy);
+				}
+			}
+
+			public GameObject GetEnemy() {
+				if (enemyPool.Count > 0) {
+					GameObject enemy = enemyPool.Dequeue();
+					enemy.SetActive(true);
+					return enemy;
+				}
+
+				GameObject newEnemy = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
+				return newEnemy;
+			}
+
+			public void ReturnEnemy(GameObject enemy) {
+				enemy.SetActive(false);
+				enemyPool.Enqueue(enemy);
+			}
+		}
+
+		// Create a gameobject and attach this script to it
+		public class NPCSpawner : MonoBehaviour {
+			[SerializeField] private float spawnInterval = 5f;
+			private float timeSinceSpawn;
+			public NPCPool_Simple objectPool;
+
+			private void Start() {
+				objectPool = FindObjectOfType<NPCPool_Simple>();
+			}
+
+			private void Update() {
+				timeSinceSpawn += Time.deltaTime;
+				if (timeSinceSpawn >= spawnInterval) {
+					GameObject enemy = objectPool.GetEnemy();
+					// Run initialization function if needed
+					enemy.transform.position = transform.position;
+					timeSinceSpawn = 0f;
+				}
+			}
+		}
+
+		// Add this script to the enemy prefab
+		public class SimpleEnemyReturn : MonoBehaviour {
+			public NPCPool_Simple objectPool;
+
+			private void Start() {
+				objectPool = FindObjectOfType<NPCPool_Simple>();
+			}
+
+			private void OnDisable() {
+				if (objectPool) objectPool.ReturnEnemy(gameObject);
+			}
+		}
+
+		// -------------------------------
+		// -------------------------------
+		// -------------------------------
+
+		// - An advanced pool that can handle multiple object types
+		// - Handles multiple object types, and can grow dynamically if needed
+		// - Essentially create a pool of pools. There may be a performance hit for this approach but won't be a problem for most games
+
+		// Create a gameobject and attach this script to it
+		public class ObjectPool_Advanced : MonoBehaviour {
+			private Dictionary<string, Queue<GameObject>> objectPool = new Dictionary<string, Queue<GameObject>>(); // Prefab name, Queue of objects
+
+			public GameObject GetObject(GameObject prefab) {
+				if (objectPool.TryGetValue(prefab.name, out Queue<GameObject> pool)) {
+					if (pool.Count == 0) {
+						return CreateNewObject(prefab);
+					} else {
+						GameObject gameObject = pool.Dequeue();
+						gameObject.SetActive(true);
+						return gameObject;
+					}
+				} else {
+					return CreateNewObject(prefab);
+				}
+			}
+
+			private GameObject CreateNewObject(GameObject prefab) {
+				GameObject gameObject = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+				gameObject.name = prefab.name; // It's important to rename the object the same as the prefab. Crucial step since we're using the name as the dictionary key
+				return gameObject;
+			}
+
+			public void ReturnGameObject(GameObject gameObject) {
+				if (objectPool.TryGetValue(gameObject.name, out Queue<GameObject> pool)) {
+					gameObject.SetActive(false);
+					pool.Enqueue(gameObject);
+				} else {
+					Queue<GameObject> newPool = new Queue<GameObject>();
+					newPool.Enqueue(gameObject);
+					objectPool.Add(gameObject.name, newPool);
+				}
+			}
+		}
+
+		// Create a gameobject and attach this script to it
+		public class Spawner_Advanced : MonoBehaviour {
+			[SerializeField] private GameObject prefab;
+			[SerializeField] private float spawnInterval = 5f;
+
+			private float timeSinceSpawn;
+			private ObjectPool_Advanced objectPool;
+
+			private void Start() {
+				objectPool = FindObjectOfType<ObjectPool_Advanced>();
+			}
+
+			private void Update() {
+				timeSinceSpawn += Time.deltaTime;
+				if (timeSinceSpawn >= spawnInterval) {
+					GameObject gameObject = objectPool.GetObject(prefab);
+					// Run initialization function if needed
+					gameObject.transform.position = transform.position;
+					timeSinceSpawn = 0f;
+				}
+			}
+		}
+
+		// Add this script to the prefab
+		// This is just an example any script can be used to return the object to the pool of course
+		public class ReturnToPool_Advanced : MonoBehaviour {
+			private ObjectPool_Advanced objectPool;
+
+			private void Start() {
+				objectPool = FindObjectOfType<ObjectPool_Advanced>();
+			}
+
+			private void OnDisable() {
+				if (objectPool) objectPool.ReturnGameObject(gameObject);
 			}
 		}
 		#endregion
